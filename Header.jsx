ee406dcdd7d7df77a0f8265f667fe3aefa37368d
Jamal <mybusineess@gmail.com>
2026-06-15 02:39:24 +0100

    create a pro

---
+++ Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+    <>
+        <nav>
+            <img src="./img/earth.png" alt="global image" className="global-img" />
+            <h1>My Travel Journal.</h1>
+        </nav>
+    </>
+    )
+}
---
+++ main.jsx
@@ -0,0 +1,11 @@
+import { createRoot } from "react-dom/client"
+import Header from "./Header"
+
+const root = createRoot(document.querySelector(".root"))
+
+
+root.render(
+    <>
+    <Header />
+    </>
+)
